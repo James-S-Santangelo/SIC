942800bf7046610a944605533c05f673f90e2e08
James Santangelo <james.santangelo37@gmail.com>
2018-12-29 15:44:21 -0500

    Main text analyses and figures complete

--- scripts/masterScript_analyses.R
+++ scripts/masterScript_analyses.R
@@ -9,44 +9,286 @@ library(lmerTest)
 library(tidyverse)
 library(vegan)
 
-#### MULTIVARIATE TRAIT CHANGE WITH URBANIZATION: FAMILY MEANS ####
+#### MULTIVARIATE TRAIT CHANGE WITH URBANIZATION: POPULATION MEANS ####
 
 # Load in family mean dataset
-familyMeans <- read_csv("data-clean/experimentalData_familyMeans.csv")
+popMeans <- read_csv("data-clean/experimentalData_popMeans.csv")
 
-familyMeans_forRDA <- familyMeans %>%
-  select(-Avg_seeds_per_flower, -n_HCN, -total_plants) %>%
-  na.omit() %>%
-  select(Population, Family_ID, Distance_to_core, Time_to_germination:freqHCN)
+# Subset popMeans datafor use in RDA
+popMeans_forRDA <- popMeans %>%
+  select(-Seeds_per_flower, -Num_Cyano) %>%
+  select(Population, Distance_to_core, Germination:FreqHCN)
 
-rda <- rda(familyMeans_forRDA %>% 
-                     select(Time_to_germination:freqHCN) ~ 
-         familyMeans_forRDA$Distance_to_core + Condition(familyMeans_forRDA$Population),
+# Perform RDA with multiple traits as response, distance as sole predictors
+rdaPop <- rda(popMeans_forRDA %>% 
+                     select(Germination:FreqHCN) ~ 
+             popMeans_forRDA$Distance_to_core,
          scale = TRUE)
-summary(rda)
-anova(rda, by = "term")
-scores(rda)$species
+summary(rdaPop)
 
-pca <- prcomp(familyMeans_forRDA %>% 
-  select(Time_to_germination:freqHCN), scale = TRUE)
-familyMeans_forRDA$pca_scores <- pca$x[,"PC1"]
+# Permutation based test of significance of distance term in RDA
+set.seed(42)
+anova.cca(rdaPop, by = "term", permutations = 1000)
 
-pca$rotation
+# Extract canonical coefficients of traits onto RDA1 (i.e. 'species scores')
+species_scores <- scores(rdaPop)$species[,"RDA1"]
+
+# Calculate cline_max according to Stock et al. 
+# Traits are first standardized by dividing by experiment-wide mean.
+# Standardized trait values are multiplied by their cannonical regression
+# coefficients on RDA. This is done for each trait and then summed. 
+popMeans <- popMeans %>%
+  mutate(clinemax = 
+           (Germination / mean(Germination)) * species_scores["Germination"] +
+           (Days_to_flower / mean(Days_to_flower)) * species_scores["Days_to_flower"] +
+           (Num_Inf / mean(Num_Inf)) * species_scores["Num_Inf"] +
+           (Reprod_biomass / mean(Reprod_biomass)) * species_scores["Reprod_biomass"] +
+           (Veget_biomass / mean(Veget_biomass)) * species_scores["Veget_biomass"] +
+           (Bnr_wdth / mean(Bnr_wdth)) * species_scores["Bnr_wdth"] +
+           (Bnr_lgth / mean(Bnr_lgth)) * species_scores["Bnr_lgth"] +
+           (Petiole_lgth / mean(Petiole_lgth)) * species_scores["Petiole_lgth"] +
+           (Peducle_lgth / mean(Peducle_lgth)) * species_scores["Peducle_lgth"] +
+           (Num_flwrs / mean(Num_flwrs)) * species_scores["Num_flwrs"] +
+           (Leaf_wdth / mean(Leaf_wdth)) * species_scores["Leaf_wdth"] +
+           (Leaf_lgth / mean(Leaf_lgth)) * species_scores["Leaf_lgth"] +
+           (Stolon_thick / mean(Stolon_thick)) * species_scores["Stolon_thick"] +
+           (FreqHCN / mean(FreqHCN)) * species_scores["FreqHCN"])
+
+# Model testing for cline in cline_max
+clineMax_mod <- lm(clinemax ~ Distance_to_core, data = popMeans)
+summary(clineMax_mod)
+
+#### POLLINATOR OBSERVATIONS ####
+
+# load data
+polldata <- read.csv('data-clean/pollinatorObservations.csv')
+
+# analysis of pollinator observations
+# impute zeros for unobserved polls
+complete_polldata <- polldata %>% 
+  group_by(Population,Morph, Distance_to_core) %>% 
+  complete(Population, Morph, fill = list(Num_Visit = 0)) %>%
+  filter(Morph != "Syrphid")
+
+# Summarize data by pollinator functional group
+# Used for visit/infl analysis
+visitshare_polldata <- complete_polldata %>% 
+  group_by(Population, Distance_to_core, Morph) %>% 
+  # calculate number of individuals observed per site per morph; number of inflorescence, number of visit per infl.
+  summarise(Num.Ind = n(),
+            Num_Visit = sum(Num_Visit),
+            Num_Inf = mean(Num_Inf)) %>%  
+  mutate(Visits_per_Inf = Num_Visit / Num_Inf) %>% 
+  # replace NA in visits/inf with zero
+  mutate(Visits_per_Inf = replace_na(Visits_per_Inf, 0))
+
+# Summarize daya by population only (merge morphs)
+# Used for total abundance analysis
+popmeans_polldata <- polldata %>% 
+  group_by(Population, Distance_to_core) %>% 
+  summarise(Num.Ind = n(),
+            Num_Visit = sum(Num_Visit),
+            Num_Inf = mean(Num_Inf)) %>% 
+  mutate(Visits_per_Inf = Num_Visit / Num_Inf)
+
+# Square root transform visits per inflorescence to improve normality of model residuals
+visitshare_polldata$sqRootVisInf <- sqrt(visitshare_polldata$Visits_per_Inf)
+
+# Multiple regression analysis on log(visit/inf) with distance, morph, and their interaction. 
+pollVisit <- lm(sqRootVisInf ~ Distance_to_core + Morph + Distance_to_core:Morph,
+         data = visitshare_polldata)
+summary(pollVisit)
+Anova(pollVisit, type = "III") #Type 3 for interpreting interaction
+
+# Diagnostic plots
+hist(residuals(pollVisit))
+par(mfrow = c(2,2))
+plot(pollVisit)
+
+#### SEEDS PER FLOWER ####
+
+# Load in seeds per flower from field-collected inflorescences
+seedFlwrFieldData <- read_csv("data-clean/flwrSeedRatio_fieldPlants.csv") %>%
+  select(-X1, -Comments) %>%
+  group_by(Population, Distance_to_core) %>%
+  summarize(Seeds_per_flower = mean(Seeds_per_flower)) 
+
+# Retrieve seeds per flower from common garden plants
+seedFlwrComGarData <- popMeans %>%
+  select(Population, Distance_to_core, Seeds_per_flower)
+
+# Combine both dataframes to run single linear model
+seedsPerFlwr <- bind_rows(seedFlwrFieldData, seedFlwrComGarData,
+                          .id = "source") %>%
+  mutate(source = fct_recode(source, "field" = "1", "common garden" = "2"))
+
+# Model testing for differences in seeds per flower among field and CG plants
+seedPerFlwrField <- lm(Seeds_per_flower ~ Distance_to_core*source, 
+                         data = seedsPerFlwr)
+summary(seedPerFlwrField)
+Anova(seedPerFlwrField, type = "III")
+plot(seedPerFlwrField) # Loogs good
+hist(residuals(seedPerFlwrField)) # Loogs good
+
+#### FIGURES ####
+
+#Theme used for plots throughout script
+ng1=theme(aspect.ratio=0.7,panel.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border=element_blank(),
+          axis.line.x = element_line(color="black",size=1),
+          axis.line.y = element_line(color="black",size=1),
+          axis.ticks=element_line(color="black"),
+          axis.text=element_text(color="black",size=15),
+          axis.title=element_text(color="black",size=1),
+          axis.title.y=element_text(vjust=2,size=17),
+          axis.title.x=element_text(vjust=0.1,size=17),
+          axis.text.x=element_text(size=15),
+          axis.text.y=element_text(size=15),
+          strip.text.x = element_text(size = 10, colour = "black",face = "bold"),
+          strip.background = element_rect(colour="black"),
+          legend.position = "top", legend.direction="vertical",
+          legend.text=element_text(size=17), legend.key = element_rect(fill = "white"),
+          legend.title = element_text(size=17),legend.key.size = unit(1.0, "cm"))
 
 
-plot(familyMeans_forRDA$Time_to_germination ~ familyMeans_forRDA$Distance_to_core)
+## MULTIVARIATE TRAIT CHANGE ##
 
-CanCorr <- CCorA(familyMeans_forRDA %>% 
-      select(Time_to_germination:freqHCN), 
-      familyMeans_forRDA$Distance_to_core)$CanCorr
+## FIGURE 2 ##
 
-familyMeans_forRDA$rda_scores <- scores(rda)$sites[,"RDA1"]
-  
-rda_mod <- lm(rda_scores ~ Distance_to_core, data = familyMeans_forRDA)
-summary(rda_mod)
+# figure 2A #
 
-pca_mod <- lm(pca_scores ~ Distance_to_core, data = familyMeans_forRDA)
-summary(pca_mod)
+# Extract RDA1 and PC1 site scores
+df_sites  <- data.frame(scores(rdaPop, display = "sites", scaling = "sites")[,1:2]) %>%
+  rownames_to_column(var = "Population") %>%
+  mutate(Population = seq(1:27)) %>%
+  left_join(., popMeans_forRDA %>% select(Population, Distance_to_core))
+
+# Extract RDA1 and PC1 species scores
+df2_species  <- data.frame(scores(rdaPop, display = "species", scaling = "species")[,1:2])     # loadings for PC1 and PC2
+row.names(df_sites) <- seq(1:27)
+
+# Plot site scores along first 2 axes. Colour points by distance
+rda_plot <- ggplot(df_sites, aes(x = RDA1, y = PC1)) + 
+  geom_point(size = 4, shape = 21, colour = "black", aes(fill = Distance_to_core)) +
+  geom_hline(yintercept = 0, linetype = "dotted") +
+  geom_vline(xintercept = 0, linetype = "dotted") +
+  scale_fill_gradient(low = "white", high = "black",
+                      limits = c(0, 50), breaks = seq(0, 50, 10)) +
+  guides(fill = guide_colorbar(ticks = FALSE,
+                               barwidth = 1.5, 
+                               barheight = 15)) +
+  ng1 + theme(legend.position = "right", 
+              legend.direction="vertical",
+              legend.text = element_text(size=15), 
+              legend.key = element_rect(fill = "white"),
+              legend.title = element_blank(),
+              legend.key.size = unit(0.5, "cm"),
+              legend.spacing.x = unit(0.1, "cm")) 
+
+# Add arrows to RDA plot. 
+rda_triplot <- rda_plot +
+  geom_segment(data = df2_species, aes(x = 0, xend = RDA1, y=0, yend = PC1), 
+               color = "black", size = 1, arrow = arrow(length = unit(0.02, "npc"))) +
+  geom_text(data = df2_species, 
+            aes(x = RDA1, y = PC1, label = rownames(df2_species),
+                hjust = 0.5 * (1 - sign(RDA1)), vjust = 0.5 * (1 - sign(PC1))), 
+            color = "black", size = 2.5) +
+  xlab("RDA1 (8.6%)") + ylab("PC1 (35%)")
+rda_triplot
+
+ggsave("analysis/figures/main-text/figure2A_RDA-triplot.pdf", 
+       plot = rda_triplot, width = 8, height = 6, unit = "in", dpi = 600)
+
+# figure 2B #
+
+clineMax_plot <- ggplot(popMeans, aes(x = Distance_to_core, y = clinemax)) +
+  geom_point(size = 4, colour = "black") +
+  geom_smooth(method = "lm", size = 2.0, colour = "black", se = FALSE) +
+  ng1
+clineMax_plot
+
+ggsave("analysis/figures/main-text/figure2B_clineMax_by_distance.pdf", 
+       plot = clineMax_plot, width = 8, height = 6, unit = "in", dpi = 600)
+
+## FIGURE 3 ##
+
+# figure 3A #
+
+cols <- c("BB"="#FF0000","HB"="#F2AD00","SB"="#5BBCD6")
+linetype <- c("BB"="dashed","HB"="dotted","SB"="dotdash")
+plotPoll <-
+  ggplot(visitshare.polldata, aes(x = Distance_to_core, y = Visits_per_Inf)) +
+  labs(x = "Source population distance\nto urban centre (km)",
+       y = "Number of pollinator visits\n per inflorescence") +
+  geom_line(data = visitshare.polldata %>% filter(Morph == "Honey Bee"),
+            stat = "smooth",
+            method = "loess",
+            se = F, 
+            # alpha = 0.7,
+            size = 1.25,
+            aes(linetype = "HB",
+                colour = "HB")) +
+  geom_line(data = visitshare.polldata %>% filter(Morph == "Bumble Bee"),
+            stat = "smooth",
+            method = "loess",
+            se = F, 
+            # alpha = 0.7,
+            size = 1.25,
+            aes(linetype = "BB",
+                colour = "BB")) +
+  geom_line(data = visitshare.polldata %>% filter(Morph == "Sweat Bee"),
+            stat = "smooth",
+            method = "loess",
+            se = F, 
+            # alpha = 0.7,
+            size = 1.25,
+            aes(linetype = "SB",
+                colour = "SB")) +
+  geom_smooth(method = "lm", size = 2, colour = "black", se = FALSE) +
+  coord_cartesian(xlim = c(2, 47), ylim = c(0, 2.5)) +
+  scale_y_continuous(breaks = seq(0, 2, 0.5)) +
+  scale_x_continuous(breaks = seq(0, 40, 10)) +
+  scale_color_manual(name = "", labels = c("Bumble bee", "Honey bee", "Sweat bee"),
+                     values = cols) +
+  scale_linetype_manual(name = "", labels = c("Bumble bee", "Honey bee", "Sweat bee"),
+                        values = linetype) +
+  ng1 + theme(legend.key.size = unit(1.5, "cm"),
+              legend.position = "top",
+              legend.direction = "horizontal")
+plotPoll
+
+ggsave("analysis/figures/main-text/figure3A_visitsPerInf_by_Distance.pdf", 
+       plot = plotPoll, width = 8, height = 6, unit = "in", dpi = 600)
+
+# figure 3B #
+
+linetypeSeedFlwrs <- c("Field" = "dashed", "CG" = "dotted")
+seedPerFlwr_plot <- ggplot(seedsPerFlwr, aes(x = Distance_to_core, y = Seeds_per_flower)) +
+  geom_smooth(method = "lm", size = 2.0, colour = "black", se = FALSE) +
+  geom_point(size = 4, alpha = 0.7, colour = "black", aes(shape = source, fill = source)) +
+  scale_shape_manual(values = c(21, 24)) +
+  scale_fill_manual(values = c("black", "white")) +
+  geom_line(data = seedsPerFlwr %>% filter(source == "field"),
+            stat = "smooth", 
+            method = "lm",
+            alpha = 0.7,
+            size = 1.5,
+            aes(linetype = "Field")) +
+  geom_line(data = seedsPerFlwr %>% filter(source == "common garden"),
+            stat = "smooth", 
+            method = "lm",
+            alpha = 0.7,
+            size = 1.5,
+            aes(linetype = "CG")) +
+  scale_linetype_manual(name = "", labels = c("Field", "Common Garden"),
+                        values = linetypeSeedFlwrs) +
+  ng1
+seedPerFlwr_plot
+
+ggsave("analysis/figures/main-text/figure3B_seeds_per_flower.pdf", 
+       plot = seedPerFlwr_plot, width = 8, height = 6, unit = "in", dpi = 600)
 
 #### MODELS FOR TRAIT CHANGE WITH URBANIZATION: ALL DATA ####
 
@@ -237,38 +479,6 @@ plot(seedPerFlwr)
 # Summarize. Distance marginal.
 summary(seedPerFlwr)
 
-#### FIGURES ####
-
-#Theme used for plots throughout script
-ng1=theme(aspect.ratio=0.7,panel.background = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border=element_blank(),
-          axis.line.x = element_line(color="black",size=1),
-          axis.line.y = element_line(color="black",size=1),
-          axis.ticks=element_line(color="black"),
-          axis.text=element_text(color="black",size=15),
-          axis.title=element_text(color="black",size=1),
-          axis.title.y=element_text(vjust=2,size=17),
-          axis.title.x=element_text(vjust=0.1,size=17),
-          axis.text.x=element_text(size=15),
-          axis.text.y=element_text(size=15),
-          strip.text.x = element_text(size = 10, colour = "black",face = "bold"),
-          strip.background = element_rect(colour="black"),
-          legend.position = "top", legend.direction="vertical",
-          legend.text=element_text(size=17), legend.key = element_rect(fill = "white"),
-          legend.title = element_text(size=17),legend.key.size = unit(1.0, "cm"))
-
-
-## MULTIVARIATE TRAIT CHANGE ##
-
-# BANNER LENGTH
-multVarTrait_plot <- ggplot(familyMeans_forRDA, aes(x = Distance_to_core, y = rda_scores)) +
-  geom_point(size = 2, position = position_dodge(width = 0.1)) +
-  geom_smooth(method = "lm", se = FALSE, colour = "black", size = 2) +
-  xlab("Distance to core (km)") + ylab("RDA1") +
-  ng1
-multVarTrait_plot
 
 ## TRAIT CHANGES WITH URBANIZATION: ALL DATA ##
 
@@ -445,22 +655,71 @@ pairs(traits, upper.panel = panel.cor,lower.panel=lsline)
 # patten with urbanization. Should these be condensed into a single traits (i.e. flower size).
 # Same is true for leaft length and leaf width.
 
-#### SEEDS PER FLOWER: FIELD DATA ####
+#### SUPPLEMENTARY ANALYSES ####
 
-seedFlwrFieldData <- read_csv("data-clean/flwrSeedRatio_fieldPlants_cleaned.csv") %>%
-  select(-X1, -Comments)
+#### RDA AND CLINEMAX FOR FAMILY MEANS ####
 
-seedPerFlwrField <- lmer(Seeds_per_flower ~ Distance_to_core + (1|Population), 
-                         data = seedFlwrFieldData)
+#### MULTIVARIATE TRAIT CHANGE WITH URBANIZATION: POPULATION MEANS ####
 
-# Plot histogram of residuals and resudials vs. fitted.
-# Hist looks good. Some fanning in fitted plot.
-seedPerFlwrField_resids <- residuals(seedPerFlwrField)
-hist(seedPerFlwrField_resids)
-plot(seedPerFlwrField)
+# Load in family mean dataset
+familyMeans <- read_csv("data-clean/experimentalData_familyMeans.csv")
 
-# Summarize. Distance marginal
-summary(seedPerFlwrField)
+# Subset popMeans datafor use in RDA
+familyMeans_forRDA <- familyMeans %>%
+  na.omit() %>%
+  select(-total_plants, -n_HCN, -Avg_seeds_per_flower) %>%
+  select(Population, Distance_to_core, Time_to_germination:freqHCN)
+
+# Perform RDA with multiple traits as response, distance as sole predictors
+rdaFam <- rda(familyMeans_forRDA %>% 
+                select(Time_to_germination:freqHCN) ~ 
+                familyMeans_forRDA$Distance_to_core,
+              scale = TRUE)
+summary(rdaFam)
+
+# Permutation based test of significance of distance term in RDA
+set.seed(42)
+anova.cca(rdaFam, by = "term", permutations = 1000)
+
+# Extract canonical coefficients of traits onto RDA1 (i.e. 'species scores')
+species_scoresFam <- scores(rdaFam)$species[,"RDA1"]
+
+# Calculate cline_max according to Stock et al. 
+# Traits are first standardized by dividing by experiment-wide mean.
+# Standardized trait values are multiplied by their cannonical regression
+# coefficients on RDA. This is done for each trait and then summed. 
+familyMeans <- familyMeans %>%
+  mutate(clinemax = 
+           (Time_to_germination / mean(Time_to_germination, na.rm = TRUE)) * species_scoresFam["Time_to_germination"] +
+           (Days_to_flower / mean(Days_to_flower, na.rm = TRUE)) * species_scoresFam["Days_to_flower"] +
+           (Num_Inf / mean(Num_Inf, na.rm = TRUE)) * species_scoresFam["Num_Inf"] +
+           (Reprod_biomass / mean(Reprod_biomass, na.rm = TRUE)) * species_scoresFam["Reprod_biomass"] +
+           (Veget_biomass / mean(Veget_biomass, na.rm = TRUE)) * species_scoresFam["Veget_biomass"] +
+           (Avg_bnr_wdth / mean(Avg_bnr_wdth, na.rm = TRUE)) * species_scoresFam["Avg_bnr_wdth"] +
+           (Avg_bnr_lgth / mean(Avg_bnr_lgth, na.rm = TRUE)) * species_scoresFam["Avg_bnr_lgth"] +
+           (Avg_petiole_lgth / mean(Avg_petiole_lgth, na.rm = TRUE)) * species_scoresFam["Avg_petiole_lgth"] +
+           (Avg_peducle_lgth / mean(Avg_peducle_lgth, na.rm = TRUE)) * species_scoresFam["Avg_peducle_lgth"] +
+           (Avg_num_flwrs / mean(Avg_num_flwrs, na.rm = TRUE)) * species_scoresFam["Avg_num_flwrs"] +
+           (Avg_leaf_wdth / mean(Avg_leaf_wdth, na.rm = TRUE)) * species_scoresFam["Avg_leaf_wdth"] +
+           (Avg_leaf_lgth / mean(Avg_leaf_lgth, na.rm = TRUE)) * species_scoresFam["Avg_leaf_lgth"] +
+           (Avg_stolon_thick / mean(Avg_stolon_thick, na.rm = TRUE)) * species_scoresFam["Avg_stolon_thick"] +
+           (freqHCN / mean(freqHCN, na.rm = TRUE)) * species_scoresFam["freqHCN"])
+
+# Model testing for cline in cline_max
+clineMax_modFam <- lm(clinemax ~ Distance_to_core, data = familyMeans)
+summary(clineMax_modFam)
+plot(clinemax ~ Distance_to_core, data = familyMeans)
+abline(clineMax_modFam)
 
 
-#### POLLINATOR OBSERVATIONS ####
+#Multiple regression analysis on abundance with distance, without controlling for inf #. 
+Ab <- lm(Num.Ind ~ Distance_to_core*Morph,
+         data = visitshare.polldata)
+summary(Ab)
+Anova(Ab, type = "III")
+
+#Multiple regression analysis on abundance with distance, controlling for inf #. 
+Ab <- lm(Num.Ind ~ Distance_to_core*Morph + Num_Inf,
+         data = visitshare.polldata)
+summary(Ab)
+Anova(Ab, type = "III")
